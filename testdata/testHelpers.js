18f240b1d820c77dadcdd89ecc8e68ed5717a49a
Tom Marrs <thomasamarrs@comcast.net>
2017-03-07 10:31:34 -0700

    Upgraded documentation in the Helpers JS file.

--- testdata/testHelpers.js
+++ testdata/testHelpers.js
@@ -2,7 +2,7 @@
 
 var testHelpers = exports;
 
-// Your code goes here.
+// Your private helper methods go here.
 
 function fullName(name) {
   return (name.first + ' ' + name.last);
@@ -13,21 +13,19 @@ function prefix() {
   return ('Mr.');
 }
 
+/* Put all your helper methods in the helpers object - it's an Object Literal in JS.
+   Your helper method name goes on the right-hand side of the ':'
+*/
 var helpers = {
   fullName: fullName,
   prefix: prefix
 };
 
+
+// Export your registerHelpers() method and use Handlebars to register all your helper methods.
 testHelpers.registerHelpers = function registerHelpers(Handlebars) {
   Handlebars.registerHelper(helpers);
 };
 
-/*
-testHelpers.registerHelpers = function registerHelpers (Handlebars) {
-  Handlebars.registerHelper('fullName', function(name) {
-    return ('fullName: ' + name.first + ' ' + name.last);
-  });
-}
-*/
 
 module.exports = testHelpers
